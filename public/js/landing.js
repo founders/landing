97b7dee097adc662b106858bd48ea6c9a337627e
Eric Mills <ericmills2@gmail.com>
2013-08-11 17:16:30 -0700

    design tweaks, prepping for heroku

--- public/js/landing.js
+++ public/js/landing.js
@@ -8,7 +8,7 @@ $(function() {
 	$('a').click(function() {
 	    var elementClicked = $(this).attr("href");
 	    var destination = $(elementClicked).offset().top;
-	    $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-15}, 500 );
+	    $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-100}, 500 );
 	    return false;
 	});
 
